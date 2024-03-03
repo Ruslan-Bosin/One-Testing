446c17a1512ebe7f78e31f3c0adf025de33320b9
Ruslan Bosin <RuslanBosin28@gmail.com>
2024-03-03 11:39:44 +0300

    render_info usage add

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from app.utils import verify_version
+from app.utils import verify_version, render_info
 
 title = "Test project"
 version = "1.0.0"
@@ -7,3 +7,5 @@ def present_Project(title: str, version: str) -> None:
     print(
         f"Project: {title} of {version} version ({'verified' if verify_version(version=version) else 'need verification'})"
     )
+    print()
+    print(render_info(version=version))
