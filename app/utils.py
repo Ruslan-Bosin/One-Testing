488aa7a568078499fda8a0ea90b7ba81780bd09b
Ruslan Bosin <RuslanBosin28@gmail.com>
2024-03-03 11:37:35 +0300

    info rendering add

--- app/utils.py
+++ app/utils.py
@@ -2,3 +2,8 @@ def verify_version(version: str) -> bool:
     if version == "1.0.0":
         return True
     return False
+
+def render_info(version: str) -> str:
+    if version == "1.0.0":
+        return "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore expedita eius quam optio illum, neque consequuntur accusamus numquam tempore odit eveniet eligendi? Error porro totam veniam, quibusdam eum accusamus atque."
+    raise Exception("Undefined version")
